8863bb683e77600120c9b3608fe4cd6f8996f1be
John Millsap <jmillsapengineer@gmail.com>
2022-11-03 16:56:36 -0500

    added comments

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 return {
     window_background_opacity = 0.7,
     font = wezterm.font 'JetBrains Mono',
+    -- changing initial size doesn't seem to be working right now...
     initial_cols = 80,
     initial_rows = 40,
 }
