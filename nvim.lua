796fc0e9c8176197f6049fa8063a53f137c04ef0
John Millsap <jmillsapengineer@gmail.com>
2022-11-03 15:42:07 -0500

    LSP config, colorscheme

--- nvim.lua
+++ nvim.lua
@@ -43,6 +43,43 @@ require('packer').startup(function(use)
 
     use 'nvim-lua/plenary.nvim'
 
+    use 'neovim/nvim-lspconfig'
+
+    use({
+        "j-hui/fidget.nvim",
+        config = function()
+            require("fidget").setup()
+        end
+    })
+
+    use 'hrsh7th/nvim-cmp'
+
+    use({
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-vsnip",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-buffer",
+        after = { "hrsh7th/nvim-cmp" },
+        requires = { "hrsh7th/nvim-cmp" },
+    })
+
+    use 'hrsh7th/vim-vsnip'
+
+    use 'simrat39/rust-tools.nvim'
+
+    use 'nvim-lua/popup.nvim'
+    
+    use {
+        'catppuccin/nvim',
+        as = 'catppuccin',
+        config = function()
+            require('catppuccin').setup {
+                flavour = 'macchiato'
+            }
+            vim.api.nvim_command 'colorscheme catppuccin'
+        end
+    }
+
     if install_plugins then
         require('packer').sync()
     end
@@ -62,3 +99,67 @@ require('nvim-tree').setup()
 if install_plugins then
    return
 end
+
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+vim.opt.shortmess = vim.opt.shortmess + "c"
+
+local function on_attach(client, buffer)
+end
+
+local opts = {
+    tools = {
+        runnables = {
+            use_telescope = true,
+        },
+        inlay_hints = {
+            auto = true,
+            show_parameter_hints = false,
+            parameter_hints_prefix = "",
+            other_hints_prefix = "",
+        },
+    },
+    server = {
+        on_attach = on_attach,
+        settings = {
+            ["rust-analyzer"] = {
+                checkOnSave = {
+                    command = "clippy",
+                },
+            },
+        },
+    },
+}
+
+require("rust-tools").setup(opts)
+
+local cmp = require("cmp")
+cmp.setup({
+    preselect = cmp.PreselectMode.None,
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        }),
+    },
+
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "vsnip" },
+        { name = "path" },
+        { name = "buffer" },
+    },
+})
